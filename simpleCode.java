7a17a8191605258b3afa3be550bf3d12588a07bd
Pandicool <51842349+Pandicool@users.noreply.github.com>
2021-02-26 09:29:50 +0530

    Update simpleCode.java

--- simpleCode.java
+++ simpleCode.java
@@ -1,1 +1,31 @@
- 
+ import java.util.Iterator;
+import java.util.Scanner;
+
+public class SimpleCode {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		char[] forward = str.toCharArray();
+		char[] rearrange = new char[str.length()];
+		int k =str.length();
+		int m =0;
+		for (int i= (k/2); i< str.length(); i++) {
+            rearrange[m++] = forward[i];
+		}
+		for (int i = 0; i <= (k/2)-1; i++) {
+			rearrange[m++] = forward[i];
+		}
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = 0; j < str.length()-i-1 ; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < i+1; j++) {
+				System.out.print(rearrange[j]);
+			}
+			System.out.println("\n");
+		}
+	}
+
+}
+
